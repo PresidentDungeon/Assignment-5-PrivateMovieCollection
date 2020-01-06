c42eb33e244eb39caffc8a24e50af5bb947a044e
pjieder <55383141+pjieder@users.noreply.github.com>
2020-01-06 14:54:59 +0100

    Title, icon

--- PrivateMovieCollection/src/privatemoviecollection/gui/PrivateMovieCollection.java
+++ PrivateMovieCollection/src/privatemoviecollection/gui/PrivateMovieCollection.java
@@ -11,7 +11,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
 
 /**
  *
@@ -29,6 +31,8 @@ public class PrivateMovieCollection extends Application
         
         stage.setScene(scene);
         stage.show();
+        stage.getIcons().add(new Image(PrivateMovieCollection.class.getResourceAsStream("image/play-button.png")));
+        stage.setTitle("EMG Film");
     }
 
     /**
